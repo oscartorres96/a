f3195a14efaf14911b50a16733541da72bf39d47
Oscar Torres <=>
2022-12-31 15:10:03 -0600

    Estructura de carpetas para comenzar con el panel de clientes

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import url
 from . import views
+from django.urls import path
+
+app_name = "admin_center"
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url('home/', views.home, name="home")
+    path('admin_panel/', views.home, name="home"),
+
+    path('admin_panel/clients', views.clients_dashboard, name="clients_dashboard")
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -6,4 +6,7 @@ def index(request):
     return HttpResponse("Hello, world. You're at the appointments index.")
 
 def home(request):
-    return render(request, "base.html")
+    return render(request, "admin_panel/main.html")
+
+def clients_dashboard(request):
+    return render(request, "clients/main.html")
